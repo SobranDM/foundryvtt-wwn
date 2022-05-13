946d360e472c2392349b3cb78c3f820e642d4fa9
SobranDM <sobran@gmail.com>
2022-05-13 15:01:55 -0700

    hail mary full of grace

--- .github/workflows/get-version.js
+++ .github/workflows/get-version.js
@@ -1,2 +1,2 @@
-var fs = require('fs');
-console.log(JSON.parse(fs.readFileSync('module.json', 'utf8')).version);
+var fs = require('fs')
+console.log(JSON.parse(fs.readFileSync('system/system.json', 'utf8')).version)
